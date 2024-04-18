b6e2e49296fa9392509dd5300f0fb7c95e4a2520
Markus Trunschke <markus.trunschke@googlemail.com>
2024-04-18 14:53:35 +0200

    Tested GNRest for R with different data. Cleaned replication code from GNR. Started GNRProdEst code for Julia

--- src/GNRProdEst.jl
+++ src/GNRProdEst.jl
@@ -1,5 +1,22 @@
-module GNRProdEst
+#####################################################
+############### THE GNRProdEst PACKAGE ##############
+# Author: Markus Trunschke                          #
+# Date of first version: 18.04.2024                 #
+# Contact: markus.trunschke@googlemail.com          #
+#####################################################
+# Notes: This package implements the estimation of  #
+#        a production function using the method de- #
+#        scribed in Gandhi, Navarro, Rivers (2020). #
+#####################################################
 
-# Write your package code here.
+module GNRProdEst
+    # Load dependencies
+    using DataFrames, Revise
+
+    ## Include other files defining functions
+    include("data_prep.jl") # Set up inputs to be in the correct for for the internals to work with
+    include("estimation_fnc.jl") # Function to estimate the production function
+
+    export Nothing
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using GNRProdEst
 using Test
+using CSV
+using DataFrames
 
 @testset "GNRProdEst.jl" begin
-    # Write your tests here.
+        data = CSV.read("C:/Users/marku/Documents/GNRProdEst/Other Programs/R-version/cd_data.csv", DataFrame)
+        @test sin(-θ) ≈ -sin(θ)
+        @test cos(-θ) ≈ cos(θ)
+        @test sin(2θ) ≈ 2*sin(θ)*cos(θ)
+        @test cos(2θ) ≈ cos(θ)^2 - sin(θ)^2
 end
