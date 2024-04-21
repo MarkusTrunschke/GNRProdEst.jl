e3eb77311443144c961a3348b886a61c776ed11e
Markus Trunschke <markus.trunschke@googlemail.com>
2024-04-21 20:33:52 +0200

    Just a comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,12 +11,8 @@ using DataFrames
         data_mis[1,5] = missing
         data_mis[!, :ln_share_m_y] = log.(select(data_mis, :M)[:,1] ./ select(data_mis, :Y)[:,1]) # Need to do the [:,1] b/c need to convert it to a vector before adding in to a column...
 
+        # Test data preperation (drop missings and add column)
         @test GNRProdEst.prep_data(data_mis; output = :Y, flex_inputs = :M, fixed_inputs = :K, intermediate_input = :M) == dropmissing!(data_mis[:, [:Y, :M, :K, :ln_share_m_y]])
 
-        # @test [1.4142135623730951, 1.4142135623730951] ≈ GNRProdEst.GNRProd()
-        
-        # @test sin(-θ) ≈ -sin(θ)
-        # @test cos(-θ) ≈ cos(θ)
-        # @test sin(2θ) ≈ 2*sin(θ)*cos(θ)
-        # @test cos(2θ) ≈ cos(θ)^2 - sin(θ)^2
+
 end
